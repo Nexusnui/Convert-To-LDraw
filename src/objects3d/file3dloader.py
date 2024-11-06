2e462b36a425fb78472fca158117268767d6a16a
Nexusnui <131207888+Nexusnui@users.noreply.github.com>
2024-11-06 21:56:23 +0100

    -moved fileloading into the ldraw_object class -deleted file3dloader.py

--- src/objects3d/file3dloader.py
+++
@@ -1,9 +0,0 @@
-import trimesh
-from src.objects3d.ldrawObject import ldraw_object
-#Todo:Add function load 3D files
-
-def load_3D_file(filepath, **kwargs) -> ldraw_object:
-    scene = trimesh.load_mesh(filepath)
-    if type(scene) != trimesh.Scene:
-        scene = trimesh.scene.scene.Scene(scene)
-    return ldraw_object(scene, **kwargs)
--- src/objects3d/ldrawObject.py
+++ src/objects3d/ldrawObject.py
@@ -1,25 +1,33 @@
 import trimesh
 import os
+
+
 class ldraw_object():
-    def __init__(self, scene: trimesh.scene.Scene, name="", bricklinknumber="", author=""):
-        self.scene = scene
+    def __init__(self, filepath: str, name="", bricklinknumber="", author=""):
+        self.__load_scene(filepath)
         self.scene.apply_scale(2.5)
         self.name = name
         self.bricklinknumber = bricklinknumber
         self.author = author
 
-    def save_dat_file(self, filepath):
+    def __load_scene(self, filepath):
+        scene = trimesh.load_mesh(filepath)
+        if type(scene) != trimesh.Scene:
+            scene = trimesh.scene.scene.Scene(scene)
+        self.scene = scene
+
+    def convert_to_dat_file(self, filepath):
         filename = os.path.basename(filepath)
-        fileheader = (f"0 FILE {filename}\n"
-                      f"0 {self.name}\n"
-                      f"0 Name:  {filename}\n"
-                      f"0 Author:  {self.author}\n"
-                      f"0 BL_Item_No {self.bricklinknumber}\n"
-                      f"0 BFC CERTIFY CCW\n")
+        header = (f"0 FILE {filename}\n"
+                  f"0 {self.name}\n"
+                  f"0 Name:  {filename}\n"
+                  f"0 Author:  {self.author}\n"
+                  f"0 BL_Item_No {self.bricklinknumber}\n"
+                  f"0 BFC CERTIFY CCW\n")
         part = self.scene.to_geometry()
         with open(filepath, "w", encoding="utf-8") as file:
-            file.write(fileheader)
-            for face in part.faces: #faces vertices
+            file.write(header)
+            for face in part.faces:  # faces vertices
                 coordinate_a = ' '.join(map(str, part.vertices[face[0]]))
                 coordinate_b = ' '.join(map(str, part.vertices[face[1]]))
                 coordinate_c = ' '.join(map(str, part.vertices[face[2]]))
